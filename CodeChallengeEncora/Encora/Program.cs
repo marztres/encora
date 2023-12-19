3d9219f447a4eff2608ac9a24921ce3ca86a728e
Marcel Solera <marcel@nubitz.io>
2023-12-18 22:04:10 -0500

    Adding Encora main project and String Manipulator Class. Console to read lines and show the result

---
+++ CodeChallengeEncora/Encora/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Encora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = new List<string>();
+            Console.WriteLine("Write the text to sort");
+
+            string line;
+            while (true)
+            {
+                line = Console.ReadLine();
+                
+                if (string.IsNullOrEmpty(line))
+                {
+                    break;
+                }
+
+                lines.Add(line);
+            }
+
+            foreach (var lineText in lines)
+            {
+                Console.WriteLine(StringManipulator.SortingOperations(lineText));
+            }
+        }
+    }
+}
---
+++ CodeChallengeEncora/Encora/StringManipulator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encora
+{
+    public static class StringManipulator
+    {
+        public static string SortingOperations(string value)
+        {
+            return new string(value.GroupBy(o => o)
+                                   .OrderByDescending(group => group.Count())
+                                   .ThenBy(group => group.Key)
+                                   .SelectMany(group => group)
+                                   .ToArray());
+        }
+    }
+}
